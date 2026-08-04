35bbdea5622d0150fe91cfa9fd7ccd5bbb597c72
qinghuan <qinghuanck@gmail.com>
2026-08-04 23:12:00 +0800

    feat: bootstrap Spring Boot application

---
+++ backend/nexus-bootstrap/src/main/java/com/nexusagent/NexusAgentApplication.java
@@ -0,0 +1,12 @@
+package com.nexusagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAgentApplication.class, args);
+    }
+}
